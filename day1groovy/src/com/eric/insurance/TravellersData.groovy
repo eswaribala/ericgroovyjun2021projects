e72f6730a1aad6c077e05a7ef9ae90c311ffe56c
eswaribala <parameswaribala@gmail.com>
2021-06-14 15:21:16 +0530

    v2

--- day1groovy/src/com/eric/insurance/TravellersData.groovy
+++ day1groovy/src/com/eric/insurance/TravellersData.groovy
@@ -1,6 +1,5 @@
 package com.eric.insurance
 
-
 //array and random number
 
 count=new Random().nextInt(100)
@@ -18,3 +17,35 @@ for(int i=0;i<count;i++) {
 travellers.sort()
 
 println "$travellers"
+
+//List
+
+countryList=[]
+
+count=new Random().nextInt(100)
+for(int i=0;i<count;i++) {
+	countryList.push("Country"+new Random().nextInt(1000))
+}
+
+//to read values from the list
+countryList.each{println it}
+
+//Map
+
+//key pair value
+//dynamic map
+travelList=[['name':'traveller1','country':'USA'],['name':'traveller2','country':'UK']]
+println "$travelList"
+travelList[0].put('duration', 10)
+travelList[1].put('duration', 20)
+//println "$travelList[0]"
+travelList.forEach{t->
+entries=t.entrySet();
+entries.forEach { entry ->
+	println "$entry.key,$entry.value"
+	
+}
+
+}
+
+
