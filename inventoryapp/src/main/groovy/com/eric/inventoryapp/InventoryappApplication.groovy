b583313fc5f6c6726f9c0b864df004d43af569b6
eswaribala <parameswaribala@gmail.com>
2021-06-15 17:24:30 +0530

    added

---
+++ inventoryapp/src/main/groovy/com/eric/inventoryapp/InventoryappApplication.groovy
@@ -0,0 +1,13 @@
+package com.eric.inventoryapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InventoryappApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(InventoryappApplication, args)
+	}
+
+}
