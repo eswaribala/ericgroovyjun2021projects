0325298d9ace0f9adf60727a1f46b046d94f4f17
eswaribala <parameswaribala@gmail.com>
2021-06-15 10:56:47 +0530

    added

---
+++ day2groovy/src/com/eric/banking/app/EmployeeApp.groovy
@@ -0,0 +1,20 @@
+package com.eric.banking.app
+
+import com.eric.banking.dao.DBHelper
+import groovy.beans.PropertyReader
+
+class EmployeeApp {
+
+	static void main(args) {
+		// TODO Auto-generated method stub
+		
+		def conn=new DBHelper().getConnection();
+		if(conn!=null)
+			println "Connected to Oracle"
+		else
+			println "Connection Could not be created"
+			
+	   		
+		
+	}
+}
---
+++ day2groovy/src/com/eric/banking/dao/DBHelper.groovy
@@ -0,0 +1,19 @@
+package com.eric.banking.dao
+import groovy.sql.Sql
+class DBHelper {
+
+	def Object getConnection() {
+		
+		Properties properties=new Properties();
+		File propertyFile=new File("./src/com/eric/banking/resources/db.properties")
+		propertyFile.withInputStream { properties.load(it) }
+		
+	    println properties.url
+		
+		def sql = Sql.newInstance(properties.url, properties.userName, properties.password, 
+			properties.driverClassName)	
+	   return sql
+	
+	}
+}
+
