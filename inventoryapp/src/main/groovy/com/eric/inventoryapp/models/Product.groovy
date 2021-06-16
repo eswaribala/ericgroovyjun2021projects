8fef77c77e129bc5c38832d1452e6e1efff5ccd2
eswaribala <parameswaribala@gmail.com>
2021-06-16 12:54:20 +0530

    added

--- inventoryapp/src/main/groovy/com/eric/inventoryapp/models/Product.groovy
+++ inventoryapp/src/main/groovy/com/eric/inventoryapp/models/Product.groovy
@@ -10,13 +10,19 @@ import javax.persistence.Id
 import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
+import org.hibernate.annotations.MetaValue
 import org.springframework.format.annotation.DateTimeFormat
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
 import groovy.transform.Canonical
+import io.swagger.annotations.ApiModel
+
 import java.time.LocalDate
 
 @Entity
 @Table(name="Eric_Product")
+@JsonIgnoreProperties(ignoreUnknown=true, value=["MetaClass","MetaMethod"])
 @Canonical class Product {
 	@Id
 	@GeneratedValue(strategy=SEQUENCE,generator="prod_seq")
