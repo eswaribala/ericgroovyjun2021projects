bff27c92ec75a3943f87ae641edb8cd810d99081
eswaribala <parameswaribala@gmail.com>
2021-06-16 17:14:48 +0530

    Added

--- day3groovy/src/com/eric/inventory/clients/XMLDataReader.groovy
+++ day3groovy/src/com/eric/inventory/clients/XMLDataReader.groovy
@@ -2,6 +2,16 @@ package com.eric.inventory.clients
 import groovy.xml.XmlSlurper
 products=new XmlSlurper().parse(new File("./src/com/eric/inventory/clients/product.xml"));
 
+println "All the Products"
 for(Object product : products.Product) {
 	println "${product.Name},->,${product.Price}"
+}
+
+println "Enter Product Id to search"
+productId=System.in.newReader().readLine()
+
+for(Object product : products.Product) {
+	//println "${product.Product_ID},${productId}"
+	if(product.Product_ID==productId)
+	  println "${product.Name},->,${product.Price}"
 }
---
+++ day3groovy/src/com/eric/inventory/clients/models/PGPProduct.groovy
@@ -0,0 +1,7 @@
+package com.eric.inventory.clients.models
+
+import groovy.transform.Canonical
+
+@Canonical class PGPProduct {
+	String productName,productImage,price
+}
